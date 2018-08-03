817646277f505bea27f6d5045e1819ac05ae4b1d
Felipe Egas <felipe.egas@schibsted.cl>
2018-08-03 15:00:14 -0400

    ff

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.post('/webhook', (req, res) => {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
+      console.log('llega mensaje', entry.messaging);
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
       // Get the sender PSID
