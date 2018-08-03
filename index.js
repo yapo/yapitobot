c564db178f0a24ca621327d7b43be52f4f4c8d4f
Felipe Egas <felipe.egas@schibsted.cl>
2018-08-03 15:31:06 -0400

    fgfgfg

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ function handleMessage(sender_psid, received_message) {
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
 
+  console.log('llega al postback', received_postback);
   let response;
 
   // Get the payload for the postback
