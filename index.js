435d953677c8cf723598f8e364b2337ff2838d33
Felipe Egas <felipe.egas@schibsted.cl>
2018-08-03 16:12:31 -0400

    fff

--- index.js
+++ index.js
@@ -126,26 +126,26 @@ function handlePostback(sender_psid, received_postback) {
       // sendTextMessage(senderID, "Postback called");
   }
   // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+  callSendAPI(sender_psid);
 
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
   // Construct the message body
-  let request_body = {
+  /* let request_body = {
     "recipient": {
       "id": sender_psid
     },
     "message": response
-  }
+  }  */
 
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: messageData
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
